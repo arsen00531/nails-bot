620235620da31a084954b70a81ca3bbf00bd3ccb
Dmitriy <dmitriycapasso@gmail.com>
2024-07-10 21:47:29 +0300

    fix stats and admin panel

--- bot/handlers/admins/panel.py
+++ bot/handlers/admins/panel.py
@@ -17,41 +17,8 @@ async def start_handler(message: types.Message, session):
         admins = admins.scalars().all()
 
     keyboard = InlineKeyboardBuilder()
-    if message.from_user.id in admins:
-        async with session() as open_session:
-            admin: models.sql.Admin = await open_session.execute(
-                select(models.sql.Admin).filter_by(id=message.from_user.id))
-            admin = admin.scalars().first()
 
-        r = requests.get(
-            f"https://api.yclients.com/api/v1/company/{admin.company_id}",
-            headers=config.YCLIENTS_HEADERS
-        )
-
-        btn_1 = InlineKeyboardButton(
-            text=f"Рассылка ФИЛИАЛ",
-            callback_data=f"broadcast_company_{admin.company_id}"
-        )
-        keyboard.row(btn_1)
-        btn_2 = InlineKeyboardButton(
-            text=f"Отзывы Яндекс",
-            callback_data=f"yandex_company_{admin.company_id}"
-        )
-        keyboard.row(btn_2)
-
-        btn = InlineKeyboardButton(
-            text="◀️ Назад",
-            callback_data="back_to_main"
-        )
-        keyboard.row(btn)
-
-        await message.answer(
-            text="<b>Админ панель</b>\n\n"
-                 f"Ваш филиал: <i>{r.json()['data']['title']}</i>",
-            reply_markup=keyboard.as_markup(),
-            parse_mode="HTML"
-        )
-    else:
+    if message.from_user.id in config.BOT_ADMINS:
         btn_2 = InlineKeyboardButton(
             text=f"Рассылка СЕТЬ",
             callback_data="broadcast_all"
@@ -87,6 +54,39 @@ async def start_handler(message: types.Message, session):
         keyboard.row(btn)
         await message.answer(
             text="<b>Админ панель</b>",
+            reply_markup=keyboard.as_markup(),
+            parse_mode="HTML"
+        )
+    elif message.from_user.id in admins:
+        async with session() as open_session:
+                admin: models.sql.Admin = await open_session.execute(
+                    select(models.sql.Admin).filter_by(id=message.from_user.id))
+                admin = admin.scalars().first()
+        r = requests.get(
+            f"https://api.yclients.com/api/v1/company/{admin.company_id}",
+            headers=config.YCLIENTS_HEADERS
+        )
+
+        btn_1 = InlineKeyboardButton(
+            text=f"Рассылка ФИЛИАЛ",
+            callback_data=f"broadcast_company_{admin.company_id}"
+        )
+        keyboard.row(btn_1)
+        btn_2 = InlineKeyboardButton(
+            text=f"Отзывы Яндекс",
+            callback_data=f"yandex_company_{admin.company_id}"
+        )
+        keyboard.row(btn_2)
+
+        btn = InlineKeyboardButton(
+            text="◀️ Назад",
+            callback_data="back_to_main"
+        )
+        keyboard.row(btn)
+
+        await message.answer(
+            text="<b>Админ панель</b>\n\n"
+                 f"Ваш филиал: <i>{r.json()['data']['title']}</i>",
             reply_markup=keyboard.as_markup(),
             parse_mode="HTML"
         )
